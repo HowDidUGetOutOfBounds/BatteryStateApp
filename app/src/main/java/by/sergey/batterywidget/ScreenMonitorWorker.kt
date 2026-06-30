a51f5705f4566d0b18d853d49716baeee7d45367
sergey K <serg.karpik@gmail.com>
2026-07-01 00:04:51 +0300

    refactoring

--- app/src/main/java/by/sergey/batterywidget/ScreenMonitorWorker.kt
+++ app/src/main/java/by/sergey/batterywidget/ScreenMonitorWorker.kt
@@ -1,6 +1,5 @@
 package by.sergey.batterywidget
 
-import android.app.KeyguardManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -9,18 +8,12 @@ import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import by.sergey.batterywidget.utills.Constants.INFO_TAG
-import java.lang.Exception
-
 
 class ScreenMonitorWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, params) {
     override fun doWork(): Result {
-
         return try {
-            Log.d(INFO_TAG, "Registering receivers from work manager")
-            registerScreenOffReceiver()
-            registerScreenOnReceiver()
-            registerUserPresentReceiver()
-
+            Log.d(INFO_TAG, "Registering battery receiver from work manager")
+            registerBatteryReceiver()
             Result.success()
         } catch (throwable: Throwable) {
             Log.e(INFO_TAG, "Error in worker")
@@ -33,66 +26,28 @@ class ScreenMonitorWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, 
         unregisterReceivers(applicationContext)
     }
 
-    private fun registerUserPresentReceiver() {
-        userPresentReceiver = object : BroadcastReceiver() {
+    private fun registerBatteryReceiver() {
+        batteryReceiver = object : BroadcastReceiver() {
             override fun onReceive(context: Context, intent: Intent) {
-
-                BatteryAppWidget.turnAlarmOnOff(context, true)
+                BatteryAppWidget.requestWidgetUpdate(context)
             }
         }
         applicationContext.registerReceiver(
-            userPresentReceiver,
-            IntentFilter(Intent.ACTION_USER_PRESENT)
+            batteryReceiver,
+            IntentFilter(Intent.ACTION_BATTERY_CHANGED)
         )
-    }
-
-    private fun registerScreenOffReceiver() {
-        screenOffReceiver = object : BroadcastReceiver() {
-            override fun onReceive(context: Context, intent: Intent) {
-
-                BatteryAppWidget.turnAlarmOnOff(context, false)
-            }
-        }
-        applicationContext.registerReceiver(
-            screenOffReceiver,
-            IntentFilter(Intent.ACTION_SCREEN_OFF)
-        )
-    }
-
-    private fun registerScreenOnReceiver() {
-        screenOnReceiver = object : BroadcastReceiver() {
-            override fun onReceive(context: Context, intent: Intent) {
-
-                val keyguardManager =
-                    context.getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
-                if (!keyguardManager.isKeyguardLocked) {
-                    BatteryAppWidget.turnAlarmOnOff(context, true)
-                }
-            }
-        }
-        applicationContext.registerReceiver(screenOnReceiver, IntentFilter(Intent.ACTION_SCREEN_ON))
     }
 
     companion object {
-        private lateinit var screenOnReceiver: BroadcastReceiver
-        private lateinit var screenOffReceiver: BroadcastReceiver
-        private lateinit var userPresentReceiver: BroadcastReceiver
+        private lateinit var batteryReceiver: BroadcastReceiver
 
         fun unregisterReceivers(context: Context) {
-
             try {
-                with(context) {
-                    Log.d(INFO_TAG, "Unregistering receivers from work manager")
-                    unregisterReceiver(screenOffReceiver)
-                    unregisterReceiver(screenOnReceiver)
-                    unregisterReceiver(userPresentReceiver)
-                }
-            }
-            catch (e: Exception)
-            {
+                Log.d(INFO_TAG, "Unregistering battery receiver from work manager")
+                context.unregisterReceiver(batteryReceiver)
+            } catch (e: Exception) {
                 Log.d(INFO_TAG, "unregisterReceivers: ${e.stackTrace}. This is different app context issue")
             }
-
         }
     }
 }
